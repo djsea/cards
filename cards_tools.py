eb9f9b8b780238e59ccac98bb7b0cc751a36dc42
dujun <djsea@139.com>
2018-03-19 17:47:32 +0800

    cards

--- cards_tools.py
+++ cards_tools.py
@@ -1,3 +1,7 @@
+# 记录所有的名片字典
+card_list = []
+
+
 def show_menu():
 
     """显示菜单"""
@@ -18,6 +22,23 @@ def new_card():
     """新增名片"""
     print("-" * 50)
     print("新增名片")
+    # 1.提示用户输入名片的详细信息
+    name_str = input("请输入姓名:")
+    phone_str = input("请输入电话:")
+    qq_str = input("请输入QQ号码:")
+    email_str = input("请输入邮箱:")
+
+    # 2.使用用户输入的信息建立一个名片字典
+    card_dict = {"name" : name_str,
+                 "phone" : phone_str,
+                 "qq" : qq_str,
+                 "email" : email_str}
+    # 3.将名片字典添加到列表中
+    card_list.append(card_dict)
+
+    print(card_list)
+    # 4.提示用户添加成功
+    print("添加 %s 的名片成功!" % name_str)
 
 
 def show_all():
@@ -26,6 +47,32 @@ def show_all():
     print("-" * 50)
     print("显示所有名片")
 
+    # 判断是否存在名片记录,如果没有,提示用户,并且返回
+    if len(card_list) == 0:
+        print("当前没有任何的名片记录,请使用新增功能添加名片!")
+        # return 可以返回一个函数的执行结果
+        # 下方的代码不会被执行
+        # 如果 return 后面没有任何的内容,标识会返回到调用函数的位置,并且不返回任何的结果
+        return
+
+    # 打印表头
+    for name in ["姓名", "电话", "QQ", "邮箱"]:
+        print(name, end="\t\t")
+
+    print("")
+
+    # 打印分割线
+    print("=" * 50)
+
+
+    # 遍历名片列表依次输出字典信息
+    for card_dict in card_list:
+
+        print("%s\t\t%s\t\t%s\t\t%s" % (card_dict["name"],
+                                        card_dict["phone"],
+                                        card_dict["qq"],
+                                        card_dict["email"]))
+
 
 def search_card():
 
@@ -33,3 +80,23 @@ def search_card():
     print("-" * 50)
     print("搜索名片")
 
+    # 1.提示用户输入要搜索的姓名
+    find_name = input("请输入要搜索的姓名:")
+
+    # 2.遍历名片列表,查询要搜索的姓名,如果没有找到,需要提示用户
+    for card_dict in card_list:
+
+        if card_dict["name"] == find_name:
+
+            print("姓名\t\t电话\t\tQQ\t\t邮箱")
+            print("=" * 50)
+            print("%s\t\t%s\t\t%s\t\t%s" % (card_dict["name"],
+                                                card_dict["phone"],
+                                                card_dict["qq"],
+                                                card_dict["email"]))
+            
+
+            break
+    else:
+
+        print("抱歉没有找到 %s " % find_name)
